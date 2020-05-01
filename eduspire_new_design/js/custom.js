bd62b931ca4d70ca6ec8219124bc7672ae944bad
rkdesign <rohitkumar199289@gmail.com>
2020-05-01 08:45:21 -0700

    add choose your goal page html

--- eduspire_new_design/js/custom.js
+++ eduspire_new_design/js/custom.js
@@ -125,6 +125,61 @@ $(document).ready(function() {
 
 });
 
+$(document).ready(function(){
+ // let scroll_link = $('.scroll');
+
+ //  //smooth scrolling -----------------------
+ //  scroll_link.click(function(e){
+ //      e.preventDefault();
+ //      let url = $('body').find($(this).attr('href')).offset().top - 95;
+ //      $('html, body').animate({
+ //        scrollTop : url
+ //      },700);
+ //      $(this).parent().addClass('active');
+ //      $(this).parent().siblings().removeClass('active');
+ //      return false; 
+ //   });
+
+ var lastId,
+    topMenu = $(".sidebar ul"),
+    topMenuHeight = topMenu.outerHeight()+50,
+
+    menuItems = topMenu.find("a"),
+
+    scrollItems = menuItems.map(function(){
+        var currentLink = $(this);
+      var item = $(currentLink.attr('href'));
+      if (item.length) { return item; }
+    });
+
+  menuItems.click(function(e){
+  var href = $(this).attr("href")
+      offsetTop = href == "#" ? 0 : $(href).offset().top - 95;
+  $('html, body').stop().animate({ 
+      scrollTop: offsetTop
+  }, 300);
+  e.preventDefault();
+  });
+
+  $(window).scroll(function(){
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       
+       menuItems.parent().removeClass("active").end().filter("[href='#"+id+"']").parent().addClass("active");
+   }                   
+  });
+});
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "627px";
     document.getElementById("side_overley").style.width = "100%";
